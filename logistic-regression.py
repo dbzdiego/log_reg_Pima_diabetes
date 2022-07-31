e39257af02189fe4e3c12da4974653dc55fe8666
Diego Alejandro Rodriguez <dar109@humboldt.edu>
2022-07-31 16:09:58 -0700

    Add files via upload

---
+++ logistic-regression.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 30 19:44:21 2022
+
+@author: diegorodriguez
+"""
+
+from csv import reader
+from math import exp
+
+# Load a CSV file
+def load_csv(filename):
+	dataset = list()
+	with open(filename, 'r') as file:
+		csv_reader = reader(file)
+		for row in csv_reader:
+			if not row:
+				continue
+			dataset.append(row)
+	return dataset
+
+# Convert string column to float
+def str_column_to_float(dataset, column):
+	for row in dataset:
+		row[column] = float(row[column].strip())
+        
+# Make a prediction with coefficients
+def predict(row, coefficients):
+	yhat = coefficients[0]
+	for i in range(len(row)-1):
+		yhat += coefficients[i + 1] * row[i]
+	return 1.0 / (1.0 + exp(-yhat))
+
+# Estimate logistic regression coefficients using stochastic gradient descent
+def coefficients_sgd(train, l_rate, n_epoch):
+	coef = [0.0 for i in range(len(train[0]))]
+	for epoch in range(n_epoch):
+		sum_error = 0
+		for row in train:
+			yhat = predict(row, coef)
+			error = row[-1] - yhat
+			sum_error += error**2
+			coef[0] = coef[0] + l_rate * error * yhat * (1.0 - yhat)
+			for i in range(len(row)-1):
+				coef[i + 1] = coef[i + 1] + l_rate * error * yhat * (1.0 - yhat) * row[i]
+		print('>epoch=%d, lrate=%.3f, error=%.3f' % (epoch, l_rate, sum_error))
+	return coef
+
+# Find the min and max values for each column
+def dataset_minmax(dataset):
+	minmax = list()
+	for i in range(len(dataset[0])):
+		col_values = [row[i] for row in dataset]
+		value_min = min(col_values)
+		value_max = max(col_values)
+		minmax.append([value_min, value_max])
+	return minmax
+
+# Rescale dataset columns to the range 0-1
+def normalize_dataset(dataset, minmax):
+	for row in dataset:
+		for i in range(len(row)):
+			row[i] = (row[i] - minmax[i][0]) / (minmax[i][1] - minmax[i][0])
+
+# load and prepare data
+filename = 'pima-indians-diabetes.csv'
+dataset = load_csv(filename)
+for i in range(len(dataset[0])):
+	str_column_to_float(dataset, i)
+
+minmax = dataset_minmax(dataset)
+normalize_dataset(dataset, minmax)
+l_rate = 0.1
+n_epoch = 1000
+coef = coefficients_sgd(dataset, l_rate, n_epoch)
+
+for row in dataset:
+	yhat = predict(row, coef)
+	print("Expected=%.3f, Predicted=%.3f [%d]" % (row[-1], yhat, round(yhat)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
